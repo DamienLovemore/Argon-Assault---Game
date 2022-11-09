510d1f7bd108e583b11d27d5a24f14f365ad4be8
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-11-09 15:34:11 -0300

    Player Ship rotation according to the input movement was added

--- Assets/Resources/Scripts/PlayerControls.cs
+++ Assets/Resources/Scripts/PlayerControls.cs
@@ -12,6 +12,15 @@ public class PlayerControls : MonoBehaviour
     [SerializeField] private float movementSpeed = 30.43f;
     [SerializeField] private float xRange = 10f;
     [SerializeField] private float yRange = 7f;
+
+    [Header("Rotation Tuning")]
+    [SerializeField] private float positionPitchFactor = -2f;
+    [SerializeField] private float controlPitchFactor = -15f;    
+    [SerializeField] private float positionYawFactor = -2.2f;
+    [SerializeField] private float controlRollFactor = -20f;  
+    
+    private float xThrow;
+    private float yThrow;
         
     //When the script becomes enabled and active, sets the keys
     //of movement to be listened
@@ -29,13 +38,24 @@ public class PlayerControls : MonoBehaviour
 
     void FixedUpdate()
     {
+        this.ShipMovement();
+        this.ShipRotation();
+    }
+
+    private void ShipMovement()
+    {
         //Reads how much left, right and up, down the player wants to go
         Vector2 playerInput = this.playerMovement.ReadValue<Vector2>();
 
+        //Stores the values of input for movement, so that rotation
+        //function can use it
+        this.xThrow = playerInput.x;
+        this.yThrow = playerInput.y;
+    
         //Separates the values into horizontal and vertical axis,
         //and tunes it with the speed and makes it framerate independent
-        float horizontalMovement = playerInput.x * this.movementSpeed * Time.deltaTime;
-        float verticalMovement = playerInput.y * this.movementSpeed * Time.deltaTime;
+        float horizontalMovement = this.xThrow * this.movementSpeed * Time.deltaTime;
+        float verticalMovement = this.yThrow * this.movementSpeed * Time.deltaTime;
 
         //Calculates where the player new position should be
         float rawXPosition = transform.localPosition.x + horizontalMovement;
@@ -49,5 +69,30 @@ public class PlayerControls : MonoBehaviour
         //Uses localPosition instead of position because the player is not a free object,
         //it is inside a parent object so it moves its position within the parents position
         transform.localPosition = new Vector3(clampedXPosition, clampedYPosition, transform.localPosition.z);
+    }
+
+    private void ShipRotation()
+    {
+        //Calculates the pitch accordinly to the position of the ship
+        float pitchDueToPosition = transform.localPosition.y * this.positionPitchFactor;
+        //Calculates the pitch accordinly to the input of the user
+        //(How much it holds the key)
+        float pitchDueToControlThrow = this.yThrow * this.controlPitchFactor;
+
+        //Rotation for the ship in the x axis
+        float pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        //Rotation for the ship in the y axis
+        //(For y axis we use just the calculation DueToPosition)
+        float yaw = transform.localPosition.x * this.positionYawFactor;
+
+        //Rotation for the ship in the z axis
+        //(For the z axis we use just the calculation DueToControlThrow)
+        float roll = this.xThrow * this.controlRollFactor;
+
+        //Uses local instead of rotation because the object is inside a parent object,
+        //it is not a free object
+        //(Euler is used to represent rotations)
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll); 
     }
 }
