7ac925e56e9eb2d7cad453fe15752fc18b0b0e81
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-11-09 12:35:54 -0300

    Enhanced movement system so that the player cannot fly off the screen

--- Assets/Resources/Scripts/PlayerControls.cs
+++ Assets/Resources/Scripts/PlayerControls.cs
@@ -5,14 +5,14 @@ using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
 {
+    [Header("Player Control Keys")]
     [SerializeField] private InputAction playerMovement;
-    [SerializeField] private float movementSpeed = 10f;
-        
-    void Start()
-    {
-        
-    }
 
+    [Header("Movement Tuning")]
+    [SerializeField] private float movementSpeed = 30.43f;
+    [SerializeField] private float xRange = 10f;
+    [SerializeField] private float yRange = 7f;
+        
     //When the script becomes enabled and active, sets the keys
     //of movement to be listened
     void OnEnable()
@@ -38,11 +38,16 @@ public class PlayerControls : MonoBehaviour
         float verticalMovement = playerInput.y * this.movementSpeed * Time.deltaTime;
 
         //Calculates where the player new position should be
-        float newXPosition = transform.localPosition.x + horizontalMovement;
-        float newYPosition = transform.localPosition.y + verticalMovement;
+        float rawXPosition = transform.localPosition.x + horizontalMovement;
+        float rawYPosition = transform.localPosition.y + verticalMovement;
+
+        //Clamps the values so that they stay in a range where the player
+        //cannot fly of the screen bounds
+        float clampedXPosition = Mathf.Clamp(rawXPosition, -xRange, xRange);
+        float clampedYPosition = Mathf.Clamp(rawYPosition, -yRange, yRange);
 
         //Uses localPosition instead of position because the player is not a free object,
         //it is inside a parent object so it moves its position within the parents position
-        transform.localPosition = new Vector3(newXPosition, newYPosition, transform.localPosition.z);
+        transform.localPosition = new Vector3(clampedXPosition, clampedYPosition, transform.localPosition.z);
     }
 }
