4a465953d505c2fb7b562955a46d434d8af695c8
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-11-11 09:01:04 -0300

    Now when the collision system is functional (when the player collides it restart the level and disables the controls)

--- Assets/Resources/Scripts/CollisionHandler.cs
+++ Assets/Resources/Scripts/CollisionHandler.cs
@@ -1,11 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
-{ 
+{
+    [SerializeField] private float delayTime = 1f;
+
+    private PlayerControls playerInputHandler;
+
+    void Start()
+    {
+        playerInputHandler = GetComponent<PlayerControls>();
+    }
+
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{this.gameObject.name} triggered {other.gameObject.name}");
+        StartCoroutine(this.PlayerShipCrash());
+    }
+
+    private IEnumerator PlayerShipCrash()
+    {
+        playerInputHandler.enabled = false;        
+        yield return new WaitForSeconds(this.delayTime);
+
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
