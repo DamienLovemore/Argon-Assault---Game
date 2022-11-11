5747321a009d516be9a9eb41ac103c3809de802f
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-11-11 08:37:29 -0300

    Now laser beams are actually functional (hit and destroy enemies)

--- Assets/Resources/Scripts/CollisionHandler.cs
+++ Assets/Resources/Scripts/CollisionHandler.cs
@@ -3,12 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
-{    
-    void OnCollisionEnter(Collision other)
-    {
-        Debug.Log($"{this.gameObject.name} bumped into {other.gameObject.name}");
-    }
-
+{ 
     void OnTriggerEnter(Collider other)
     {
         Debug.Log($"{this.gameObject.name} triggered {other.gameObject.name}");
