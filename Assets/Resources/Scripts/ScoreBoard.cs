6d5a185c26aebbeccb7dd06ecf5653ac3e39c66c
DamienLovemore <matheusoaresmartins2020@gmail.com>
2022-11-11 14:23:54 -0300

    Player score system is finished, and game HUD was added

--- Assets/Resources/Scripts/ScoreBoard.cs
+++ Assets/Resources/Scripts/ScoreBoard.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ScoreBoard : MonoBehaviour
 {
     private int gameScore;
+    private TextMeshProUGUI textPlayerScore;
+
+    //Gathers all the GameObjects references it needs to work
+    private void Start()
+    {
+        textPlayerScore = GetComponent<TextMeshProUGUI>();
+        textPlayerScore.text = "Start";
+    }
 
     //Increases how much score the player currently have
     public void IncreaseScore(int scorePoints)
     {
         this.gameScore += scorePoints;
-        Debug.Log($"Score is now: {this.gameScore}");
+        textPlayerScore.text = $"{this.gameScore}";
     }
 }
